b3ffc25a34079ce5232eaaa5ea07e063babcbb33
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-03-13 17:12:38 +0300

    Add entities to project

---
+++ Library.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid AuthorId { get; set; }
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public DateTime TakeDateTime { get; set; }
+        public  DateTime ReturnDateTime { get; set; }
+        public  BookAvailability Status { get; set; }
+    }
+}
---
+++ Library.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
